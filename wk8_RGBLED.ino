0067cbac6fba399adbecf62716c7c11fa7a49682
1812ava <1812ava@gmail.com>
2016-11-02 15:56:30 +0800

    Add files via upload

---
+++ wk8_RGBLED.ino
@@ -0,0 +1,31 @@
+int greenPin = 6;
+int redPin = 9;
+int bluePin = 5;
+int counter = 0;
+
+void setup() {
+  pinMode(greenPin,OUTPUT);
+  pinMode(redPin,OUTPUT);
+  pinMode(bluePin,OUTPUT);
+}
+
+void loop() {
+  counter++;
+  if(counter == 1){//紅綠同時點亮，產生黃色
+    analogWrite(greenPin,255);
+    analogWrite(redPin,240);
+    analogWrite(bluePin,0);
+  }
+  else if(counter == 2){//藍紅同時點亮，產生紫色
+    analogWrite(greenPin,0);
+    analogWrite(redPin,230);
+    analogWrite(bluePin,255);
+  }
+  else if(counter == 3){//藍綠同時點亮，產生青色
+    analogWrite(greenPin,235);
+    analogWrite(redPin,0);
+    analogWrite(bluePin,235);
+    counter = 0;
+  }
+  delay(1000);
+}
